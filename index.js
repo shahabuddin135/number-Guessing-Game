e5877a119a15d73993e73519f7f4b3b46c8ce49e
shahabuddin135 <voyagersvrs135@gmail.com>
2024-03-23 00:33:07 +0500

    version 1

---
+++ index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let randomNumber = Math.floor(Math.random() * 10 + 1);
+let numGame;
+console.log(chalk.bgCyan("Guess a number from 1 to 10"));
+do {
+    numGame = await inquirer.prompt([
+        {
+            name: "userGuess",
+            type: "number",
+            message: "Enter your Guessed number",
+        },
+    ]);
+    if (numGame.userGuess === randomNumber) {
+        console.log(chalk.bgGreen(`${randomNumber} is the correct answer!`));
+    }
+    else if (numGame.userGuess > randomNumber) {
+        console.log(chalk.bgRed(`Yes!!  ${numGame.userGuess} is too high!`));
+    }
+    else if (numGame.userGuess < randomNumber) {
+        console.log(chalk.bgBlueBright(`${numGame.userGuess} is too low!`));
+    }
+} while (numGame.userGuess !== randomNumber);
